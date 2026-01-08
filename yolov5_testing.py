40f993b9e04cf9de369c162f17434238c5a5c26b
Juanita Pombo <160296107+juanitapombo@users.noreply.github.com>
2026-01-08 11:30:15 -0800

    Add files via upload

---
+++ yolov5_testing.py
@@ -0,0 +1,7 @@
+#%%
+import torch
+
+model = torch.hub.load("ultralytics/yolov5", "yolov5n")
+
+# %%
+img = '/Users/juanitapombog/Desktop/DUKE/classes/appls_ML/module5_replication/Dataset/nDFS_Dataset/Raw_TEM_DataSet_Annotated/images/train1/1.jpg'
